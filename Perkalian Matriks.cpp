19067973001a7b8a445365639bb41dfcb6ceae9f
Mokhammad Brian Yusuf <72732482+MBY-dotcom@users.noreply.github.com>
2021-02-16 09:26:54 +0700

    Add files via upload

---
+++ Perkalian Matriks.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+using namespace std;
+
+void matriks()
+
+{
+
+
+        int A[10][10];
+
+        int B[10][10];
+
+        int C[10][10];
+
+        int bar,kolA,kolB,kolC,kol;
+
+
+    
+
+        cout<<"\t\t       PROGRAM PENJUMLAHAN ARRAY 2 DIMENSI         "<<endl;
+
+
+
+        cout<<"Masukkan Nilai Matriks A: "<<endl;
+
+        cout<<"=========================="<<endl;
+
+
+            for(bar=1;bar<=2;bar++)
+
+            {
+
+                for(kol=1;kol<=2;kol++)
+
+                {
+
+                    cout<<"A["<<bar<<"]["<<kol<<"] =";
+
+                    cin>>A[bar][kol];
+
+
+                }
+
+            }
+
+            cout<<endl<<endl;
+
+            cout<<"Masukkan Nilai Matriks B: "<<endl;
+
+        cout<<"=========================="<<endl;
+
+
+            for(bar=1;bar<=2;bar++)
+
+            {
+
+                for(kol=1;kol<=2;kol++)
+
+                {
+
+                    cout<<"B["<<bar<<"]["<<kol<<"] =";
+
+                    cin>>B[bar][kol];
+
+                    C[bar][kol]=(A[bar][kol]+B[bar][kol]);
+
+
+                }
+
+            }
+
+            system("cls");
+
+
+            cout<<"Hasil Penjumlahan = "<<endl;
+
+
+                for(bar=1;bar<=2;bar++)
+
+                {
+
+                    cout<<"|";
+
+                    for(kolA=1;kolA<=2;kolA++)
+
+                    {
+
+                        cout<<""<<A[bar][kolA];
+
+                    }
+
+                    cout<<"|";
+
+                    cout<<"|";
+
+                    for(kolB=1;kolB<=2;kolB++)
+
+                         {
+
+                        cout<<""<<B[bar][kolB];
+
+                    }
+
+                    cout<<"|";
+
+                    cout<<"|";
+
+
+                    for(kolC=1;kolC<=2;kolC++)
+
+                         {
+
+                        cout<<""<<C[bar][kolC];
+
+                    }
+
+                    cout<<"|";
+
+                    cout<<""<<endl;
+
+                }
+
+
+    }
+
+
+main()
+
+{
+
+    matriks();
+
+}
